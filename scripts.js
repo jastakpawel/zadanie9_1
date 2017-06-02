ac21c4a76dde4babe3777e48284ccd5a1d00dcfb
jastakpawel <innyjastak@gmail.com>
2017-06-02 20:00:02 +0200

    first commit

---
+++ scripts.js
@@ -0,0 +1,18 @@
+function getTriangleArea(a, h) {
+	
+	if( (a <= 0) || (h <= 0) ) {
+		console.log('Nieprawidłowe dane')
+	} else {
+		return a * h / 2
+	}
+}
+
+console.log( getTriangleArea(10, 6) )
+
+var triangle1Area = getTriangleArea(10, 15); 
+var triangle2Area = getTriangleArea(15, 48);
+var triangle3Area = getTriangleArea(45, 69);
+
+console.log ('Pole trójkąta wynosi ' + triangle1Area);
+console.log ('Pole trójkąta wynosi ' + triangle2Area);
+console.log ('Pole trójkąta wynosi ' + triangle3Area);
